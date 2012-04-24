58ab7d39cd46e4826a2caf7709fb1d225c958815
zk <zk546113096@gmail.com>
2012-04-24 14:17:09 +0800

    js  $符号 换成jQuery

--- sources/js/common.js
+++ sources/js/common.js
@@ -1,53 +1,53 @@
-  $.fn.toggleInputText = function() { /* toggle input {{{ */
-    var v = $(this).val(); 
-    if($(this).val() == "")
-      $(this).val(v).css({'color' : '#898888'});
-    $(this).focus(function(){
-      $(this).val('').select().css({'color' : '#000'});
+  jQuery.fn.toggleInputText = function() { /* toggle input {{{ */
+    var v = jQuery(this).val(); 
+    if(jQuery(this).val() == "")
+      jQuery(this).val(v).css({'color' : '#898888'});
+    jQuery(this).focus(function(){
+      jQuery(this).val('').select().css({'color' : '#000'});
     }).blur(function(){
-      if($(this).val()==""){
-         $(this).val(v).css({'color' : '#898888'});
+      if(jQuery(this).val()==""){
+         jQuery(this).val(v).css({'color' : '#898888'});
       }
     });
   };/* }}} */
-$(document).ready(function() {
-  $('.header-search-tabs > li').click(function() {/* 首页 搜索框 切换 {{{ */
-    $(this).addClass('current').siblings().removeClass('current');
+jQuery(document).ready(function() {
+  jQuery('.header-search-tabs > li').click(function() {/* 首页 搜索框 切换 {{{ */
+    jQuery(this).addClass('current').siblings().removeClass('current');
 
-    var idx = $('.header-search-tabs > li').index(this);
-    $(this).parents('#header-search').css(
+    var idx = jQuery('.header-search-tabs > li').index(this);
+    jQuery(this).parents('#header-search').css(
       'background', 'url("./sources/images/base/header-search-' + idx + '.png") no-repeat' 
     );
-    $('.header-search-blocks > li:eq(' + idx + ')').addClass('current').siblings().removeClass('current');
+    jQuery('.header-search-blocks > li:eq(' + idx + ')').addClass('current').siblings().removeClass('current');
     return false;
   });/* }}} */
-  $('.switch-tabs > li').mouseover(function() {/* tab 鼠标经过切换 切换 {{{ */
-    $(this).addClass('current').siblings().removeClass('current');
+  jQuery('.switch-tabs > li').mouseover(function() {/* tab 鼠标经过切换 切换 {{{ */
+    jQuery(this).addClass('current').siblings().removeClass('current');
 
-    var idx = $('.switch-tabs > li').index(this);
-    $('.switch-blocks > li:eq(' + idx + ')').addClass('current').siblings().removeClass('current');
+    var idx = jQuery('.switch-tabs > li').index(this);
+    jQuery('.switch-blocks > li:eq(' + idx + ')').addClass('current').siblings().removeClass('current');
     return false;
   });/* }}} */
-  $('.header-01 ul.menu li.down').hover(function() { /* header-01 子菜单 显示 隐藏 {{{ */
-    $(this).find('dl').fadeIn('slow'); 
-    $(this).addClass('active');
+  jQuery('.header-01 ul.menu li.down').hover(function() { /* header-01 子菜单 显示 隐藏 {{{ */
+    jQuery(this).find('dl').fadeIn('slow'); 
+    jQuery(this).addClass('active');
     return false;
   }, function() {
-    $(this).find('dl').fadeOut('slow'); 
-    $(this).removeClass('active');
+    jQuery(this).find('dl').fadeOut('slow'); 
+    jQuery(this).removeClass('active');
     return false;
   }); /* }}} */
-  $('.header-01 .search-type').hover(function() { /* header-01 search type 显示 隐藏 {{{ */
-    $(this).find('dl').fadeIn('slow'); 
-    $('.header-01 .search-type dl dt').hover(function() {
-      $(this).addClass('active'); 
+  jQuery('.header-01 .search-type').hover(function() { /* header-01 search type 显示 隐藏 {{{ */
+    jQuery(this).find('dl').fadeIn('slow'); 
+    jQuery('.header-01 .search-type dl dt').hover(function() {
+      jQuery(this).addClass('active'); 
     }, function() {
-      $(this).removeClass('active'); 
+      jQuery(this).removeClass('active'); 
     });
   }, function() {
-    $(this).find('dl').fadeOut('slow'); 
+    jQuery(this).find('dl').fadeOut('slow'); 
   }); /* }}} */
 
-  $('.header-01 .search-text input').toggleInputText();
-  $('.footer-links-text').toggleInputText();
+  jQuery('.header-01 .search-text input').toggleInputText();
+  jQuery('.footer-links-text').toggleInputText();
 });
--- sources/js/sns-daren.js
+++ sources/js/sns-daren.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
-  $('.sns-daren-block ul li').hover(function() {
-    $(this).addClass('bd1sb40014'); 
+jQuery(document).ready(function() {
+  jQuery('.sns-daren-block ul li').hover(function() {
+    jQuery(this).addClass('bd1sb40014'); 
   }, function() {
-    $(this).removeClass('bd1sb40014'); 
+    jQuery(this).removeClass('bd1sb40014'); 
   });
 });
