25955f7b941a4065384c3f084e256f9904bff759
zk <zk546113096@gmail.com>
2012-04-24 14:09:56 +0800

    增加社区达人 页面

---
+++ sources/js/sns-daren.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $('.sns-daren-block ul li').hover(function() {
+    $(this).addClass('bd1sb40014'); 
+  }, function() {
+    $(this).removeClass('bd1sb40014'); 
+  });
+});
